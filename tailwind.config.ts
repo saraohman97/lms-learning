c0d7d3bb118c089b5eb0b1719a142b761a831728
Sara Öhman <saraohman97@gmail.com>
2024-04-02 19:27:21 +0200

    Upload thing not working.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from "tailwindcss"
+import { withUt } from 'uploadthing/tw'
 
-const config = {
+// const { withUt } = require("uploadthing/tw")
+
+const config = withUt({
   darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
@@ -75,6 +78,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+}) satisfies Config
 
 export default config
